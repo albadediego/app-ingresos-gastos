99ad13d71bbd907ad1ca40cb3b3a1556feb9ea47
Alba <albadediegocorrea@gmail.com>
2025-09-21 19:01:02 +0200

    actualizar insert, terminar de modificar uupdate y separar capa models

--- app_ingresos_gastos/models.py
+++ app_ingresos_gastos/models.py
@@ -1,15 +1,93 @@
+from app_ingresos_gastos import MOVIMIENTOS_FILE, LAST_ID_FILE
+import csv
+
 def select_all():
-    pass
-    #devolver una lista con todos los registros del fichero MOVIMIENTOS_FILE
+    datos=[]
+    #Llamada al archivo csv
+    fichero = open(MOVIMIENTOS_FILE, 'r')
+    csvReader = csv.reader(fichero, delimiter=',',quotechar='"')
+    for items in csvReader:
+            datos.append(items)
+    fichero.close()
 
-def select_by():
-    pass
-    #devolver un registro con el id dado o vacio si no lo encuentra en MOVIMIENTOS_FILE
+    return datos
 
-def delete_by(id):
-    pass
-    #borra el registro con el id dado
+def select_by(id,condicion):
+        miFicheroBuscado = open(MOVIMIENTOS_FILE, 'r')
+        lecturaFichero = csv.reader(miFicheroBuscado, delimiter=',',quotechar='"')
+        registroBuscado = []
+        for item in lecturaFichero:
+            if condicion == "==":
+                if item[0] == str(id):
+                    #encuentro el id buscado de mi registro
+                    registroBuscado = item
+            elif condicion == "!=":
+                if item[0] != str(id):
+                  registroBuscado.append(item)
+            elif condicion == "dic":
+                if item[0] == str(id):
+                    registroBuscado = dict()
+                    registroBuscado['id']=item[0]
+                    registroBuscado['fecha']=item[1]
+                    registroBuscado['concepto']=item[2]
+                    registroBuscado['monto']=item[3]
 
-def insert():
-    pass
-    #crea un nuevo registro con un nuevo id
+        miFicheroBuscado.close()
+        
+        return registroBuscado
+
+def delete_by(id, registros):
+    fichero_guardar= open(MOVIMIENTOS_FILE,'w',newline='')
+    #llamar al metodo writer de escritura y configuramos el formato
+    csvWriter = csv.writer(fichero_guardar,delimiter=',',quotechar='"')
+    #registramos los datos recibidos en el archivo csv
+    for datos in registros:
+        csvWriter.writerow(datos)
+    
+    fichero_guardar.close()
+
+def insert(requestForm):
+    #######################Generar el nuevo id para registro###################################
+    listaId = []
+    lastId = ""
+    newId = 0
+    ficheroId = open(LAST_ID_FILE, 'r')
+    csvReaderId = csv.reader(ficheroId, delimiter=',', quotechar='"')
+    for items in csvReaderId:
+        listaId.append(items[0])
+    if listaId == []:
+        newId = 1
+    else:    
+        lastId = listaId[-1] #obtenemos el ultimo id registrado
+        newId = int(lastId) + 1
+
+    ficheroId.close()
+    ######################Guardar el anterior id en last_id.csv#######################################
+    fichero_new_id=open(LAST_ID_FILE, 'w')
+    fichero_new_id.write(str(newId))
+    fichero_new_id.close()
+
+    #######################################################################
+    #Acceder al archivo y configurar para la carga del nuevo registro
+    mifichero = open(MOVIMIENTOS_FILE, 'a',newline="")
+    #Llamar al método writer de escritura y configuramos el formato
+    lectura = csv.writer(mifichero,delimiter=',',quotechar='"')
+    #Registramos los datos recibidos en el archivo csv
+    lectura.writerow([newId,requestForm['fecha'],requestForm['concepto'],requestForm['monto']])
+    mifichero.close()
+
+def update_by(id, registros, requestForm):
+    nuevosDatos = []
+    for item in registros:
+        if item[0] == str(id):
+            nuevosDatos.append([id, requestForm['fecha'], requestForm['concepto'], requestForm['monto'] ])
+        else:
+            nuevosDatos.append(item)
+
+    fichero_update= open(MOVIMIENTOS_FILE,'w',newline='')
+    #llamar al metodo writer de escritura y configuramos el formato
+    csvWriter = csv.writer(fichero_update,delimiter=',',quotechar='"')
+    #registramos los datos recibidos en el archivo csv
+    csvWriter.writerows(nuevosDatos)
+    
+    fichero_update.close()
--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -2,16 +2,11 @@ from app_ingresos_gastos import app, MOVIMIENTOS_FILE, LAST_ID_FILE
 from flask import render_template, request, redirect
 import csv
 from datetime import date
+from app_ingresos_gastos.models import *
 
 @app.route("/")
 def index():
-    datos=[]
-    #Llamada al archivo csv
-    fichero = open(MOVIMIENTOS_FILE, 'r')
-    csvReader = csv.reader(fichero, delimiter=',',quotechar='"')
-    for items in csvReader:
-            datos.append(items)
-    fichero.close()
+    datos = select_all()
     return render_template("index.html", data=datos, titulo="Lista")
 
 @app.route("/new", methods=["GET", "POST"])
@@ -22,30 +17,7 @@ def new():
         if comprobarErrores:
               return render_template("new.html", titulo="Nuevo", tipoAccion="Registro", tipoBoton="Guardar", errors=comprobarErrores, dataForm=request.form)
         else:
-            #######################Generar el nuevo id para registro###################################
-            listaId = []
-            lastId = ""
-            newId = 0
-            ficheroId = open(LAST_ID_FILE, 'r')
-            csvReaderId = csv.reader(ficheroId, delimiter=',', quotechar='"')
-            for items in csvReaderId:
-                listaId.append(items[0])
-            lastId = listaId[-1] #obtenemos el ultimo id registrado
-            newId = int(lastId) + 1
-            ficheroId.close()
-            ######################Guardar el anterior id en last_id.csv#######################################
-            fichero_new_id=open(LAST_ID_FILE, 'w')
-            fichero_new_id.write(str(newId))
-            fichero_new_id.close()
-
-            #######################################################################
-            #Acceder al archivo y configurar para la carga del nuevo registro
-            mifichero = open(MOVIMIENTOS_FILE, 'a',newline="")
-            #Llamar al método writer de escritura y configuramos el formato
-            lectura = csv.writer(mifichero,delimiter=',',quotechar='"')
-            #Registramos los datos recibidos en el archivo csv
-            lectura.writerow([newId,request.form['fecha'],request.form['concepto'],request.form['monto']])
-            mifichero.close()
+            insert(request.form)
 
             #Redireccionar a home o lista de registros
             return redirect("/")
@@ -55,52 +27,29 @@ def new():
 @app.route("/delete/<int:id>", methods=["GET", "POST"])
 def delete(id):
     if request.method == 'GET':
-        miFicheroDelete = open(MOVIMIENTOS_FILE, 'r')
-        lecturaDelete = csv.reader(miFicheroDelete, delimiter=',',quotechar='"')
-        registroBuscado = []
-        for item in lecturaDelete:
-            if item[0] == str(id):
-                #encuentro el id buscado de mi registro
-                registroBuscado = item
+        registroBuscado = select_by(id,"==")
+     
         return render_template("delete.html",titulo="Borrar",data=registroBuscado)
     else: #POST 
          ############Lectura de archivo csv para quitar todos los datos excepto el id#########
-        fichero_lectura = open(MOVIMIENTOS_FILE,'r')
-        csvReader = csv.reader(fichero_lectura,delimiter=',',quotechar='"')
-        registros=[]
-
-        for item in csvReader:
-             if item[0] != str(id):
-                  registros.append(item)
-        fichero_lectura.close()
+        registros=select_by(id,"!=")
 
         ###################Guardar el registro de datos obtenido#############################
-        fichero_guardar= open(MOVIMIENTOS_FILE,'w',newline='')
-        #llamar al metodo writer de escritura y configuramos el formato
-        csvWriter = csv.writer(fichero_guardar,delimiter=',',quotechar='"')
-        #registramos los datos recibidos en el archivo csv
-        for datos in registros:
-             csvWriter.writerow(datos)
-        
-        fichero_guardar.close()
-
-
+        delete_by(id,registros)
         return redirect('/')
 
 @app.route("/update/<int:id>", methods=["GET","POST"])
 def update(id):
     if request.method == "POST":
-        return f"Se debe actualizar estos datos {request.form}"
+        comprobarErrores = validarFormulario(request.form)
+
+        if comprobarErrores:
+            return render_template("update.html", titulo="Actualizar", tipoAccion="Actualizacion", tipoBoton="Editar", dataForm=request.form, errors=comprobarErrores)
+        
+        update_by(id, select_all(), request.form)
+        return redirect("/")
     else:
-        miFicheroUpdate = open(MOVIMIENTOS_FILE, 'r')
-        lecturaUpdate = csv.reader(miFicheroUpdate, delimiter=',', quotechar='"')
-        registro_buscado = dict()
-        for item in lecturaUpdate:
-            if item[0] == str(id):
-                registro_buscado['id']=item[0]
-                registro_buscado['fecha']=item[1]
-                registro_buscado['concepto']=item[2]
-                registro_buscado['monto']=item[3]
+        registro_buscado=select_by(id,"dic")
 
         return render_template("update.html", titulo="Actualizar", tipoAccion="Actualizacion", tipoBoton="Editar", dataForm=registro_buscado)
 
